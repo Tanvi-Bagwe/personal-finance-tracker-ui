aae300fab919361f5ea1f32b6c3d3cb7d332f03d
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-14 16:55:01 +0000

    component for login

---
+++ src/app/features/auth/login/login.ts
@@ -0,0 +1,46 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../../core/services/auth-service';
+import { Router } from '@angular/router';
+import { MatCard } from '@angular/material/card';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
+import { MatInput } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButton } from '@angular/material/button';
+import { Header } from '../../../layout/header/header';
+
+@Component({
+  selector: 'app-login',
+  imports: [
+    MatCard,
+    MatFormField,
+    MatLabel,
+    MatFormField,
+    MatInput,
+    FormsModule,
+    MatButton,
+    Header,
+  ],
+  templateUrl: './login.html',
+  styleUrl: './login.css',
+})
+export class Login {
+  protected password: any;
+  protected username: any;
+  constructor(
+    private readonly auth: AuthService,
+    private readonly router: Router,
+  ) {}
+
+  login() {
+    this.auth
+      .login({
+        username: this.username,
+        password: this.password,
+      })
+      .subscribe((res: any) => {
+        this.auth.storeTokens(res);
+
+        this.router.navigate(['/dashboard']);
+      });
+  }
+}
---
+++ src/app/layout/sidebar/sidebar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list'; // For mat-nav-list
+import { MatIconModule } from '@angular/material/icon'; // For mat-icon
+import { RouterLink, RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  imports: [MatListModule, MatIconModule, RouterModule, RouterLink],
+  templateUrl: './sidebar.html',
+  styleUrl: './sidebar.css',
+})
+export class Sidebar {}

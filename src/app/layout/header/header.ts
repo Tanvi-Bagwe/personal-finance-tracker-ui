b9325236279c6bbbb5d44f4eec743c010ae0fb2d
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-17 12:11:14 +0000

    signal store

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,7 +1,8 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
+import { AppStore } from '../../shared/service/app-store/app-store.service';
 
 @Component({
   selector: 'app-header',
@@ -10,17 +11,8 @@ import { MatToolbar } from '@angular/material/toolbar';
   styleUrl: './header.scss',
 })
 export class Header {
-  darkMode = signal(false);
-
-  toggleDarkMode() {
-    this.darkMode.update((v) => !v);
-    const host = document.documentElement; // or document.body
-    if (this.darkMode()) {
-      host.classList.add('theme-dark');
-      host.classList.remove('theme-light');
-    } else {
-      host.classList.add('theme-light');
-      host.classList.remove('theme-dark');
-    }
+  constructor(readonly appStore: AppStore) {
+    console.log(this.appStore.isLoggedIn());
   }
+
 }

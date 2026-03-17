d851f6c97f07d188527020808cf7a3f4c485e4da
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-17 10:37:18 +0000

    working till dark mode

--- src/app/layout/sidebar/sidebar.ts
+++ src/app/layout/sidebar/sidebar.ts
@@ -1,12 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnDestroy, signal } from '@angular/core';
 import { MatListModule } from '@angular/material/list'; // For mat-nav-list
 import { MatIconModule } from '@angular/material/icon'; // For mat-icon
-import { RouterLink, RouterModule } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {
+  MatSidenav,
+  MatSidenavContainer,
+  MatSidenavContent,
+  MatSidenavModule,
+} from '@angular/material/sidenav';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { MediaMatcher } from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [MatListModule, MatIconModule, RouterModule, RouterLink],
+  imports: [
+    MatListModule,
+    MatIconModule,
+    RouterModule,
+    MatSidenavContent,
+    MatSidenavContainer,
+    MatToolbar,
+    MatIconButton,
+    MatSidenav,
+    MatSidenavModule,
+  ],
   templateUrl: './sidebar.html',
   styleUrl: './sidebar.scss',
 })
-export class Sidebar {}
+export class Sidebar implements OnDestroy {
+  protected readonly fillerNav = Array.from({ length: 50 }, (_, i) => `Nav Item ${i + 1}`);
+
+  protected readonly isMobile = signal(true);
+
+  private readonly _mobileQuery: MediaQueryList;
+  private readonly _mobileQueryListener: () => void;
+
+  constructor() {
+    const media = inject(MediaMatcher);
+
+    this._mobileQuery = media.matchMedia('(max-width: 600px)');
+    this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQueryListener = () => this.isMobile.set(this._mobileQuery.matches);
+    this._mobileQuery.addEventListener('change', this._mobileQueryListener);
+  }
+
+  darkMode = signal(false);
+
+  toggleDarkMode() {
+    this.darkMode.update((v) => !v);
+    const host = document.documentElement; // or document.body
+    if (this.darkMode()) {
+      host.classList.add('theme-dark');
+      host.classList.remove('theme-light');
+    } else {
+      host.classList.add('theme-light');
+      host.classList.remove('theme-dark');
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._mobileQuery.removeEventListener('change', this._mobileQueryListener);
+  }
+}

deb5c4a8cbb75d84790b9403566c47b45a33664c
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-20 19:36:24 +0000

    user session verify updated

--- src/app/layout/dashboard-layout/dashboard-layout.ts
+++ src/app/layout/dashboard-layout/dashboard-layout.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { MatListModule } from '@angular/material/list'; // For mat-nav-list
 import { MatIconModule } from '@angular/material/icon'; // For mat-icon
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { Sidebar } from '../sidebar/sidebar';
+import { ApiService } from '../../shared/service/api/api-service';
+import { AppStore } from '../../shared/service/app-store/app-store.service';
+import { AuthResponse } from '../../shared/models/auth-models';
 
 @Component({
   selector: 'app-dashboard-layout',
@@ -10,4 +13,36 @@ import { Sidebar } from '../sidebar/sidebar';
   templateUrl: './dashboard-layout.html',
   styleUrl: './dashboard-layout.scss',
 })
-export class DashboardLayout {}
+export class DashboardLayout implements OnInit {
+  constructor(
+    private readonly api: ApiService,
+    private readonly store: AppStore,
+    private readonly router: Router,
+  ) {}
+
+  ngOnInit() {
+    this.validateSession();
+  }
+
+  validateSession() {
+    const payload: AuthResponse = {
+      access: this.store.access() ?? '',
+      refresh: this.store.refresh() ?? '',
+    };
+
+    this.api.verifySession(payload).subscribe({
+      next: (response: any) => {
+        if (response.status === 'refreshed') {
+          payload.access = response.access;
+          this.store.setAuth(payload);
+        }
+      },
+      error: (err) => {
+        // SCENARIO 3: Both tokens are invalid/expired or malformed
+        console.error('Session dead:', err);
+        this.store.logout();
+        this.router.navigate(['/login']);
+      },
+    });
+  }
+}

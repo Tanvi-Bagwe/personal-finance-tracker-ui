9072c1752f0b00c74093a609392541007b5f1367
Tanvi-Bagwe <tanvibagwe97@gmail.com>
2026-03-20 19:36:31 +0000

    user session verify updated

--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -3,16 +3,23 @@ import { MatIcon } from '@angular/material/icon';
 import { MatIconButton } from '@angular/material/button';
 import { MatToolbar } from '@angular/material/toolbar';
 import { AppStore } from '../../shared/service/app-store/app-store.service';
+import { MatMenu, MatMenuItem, MatMenuTrigger } from '@angular/material/menu';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [MatIcon, MatIconButton, MatToolbar],
+  imports: [MatIcon, MatIconButton, MatToolbar, MatMenu, MatMenuTrigger, MatMenuItem, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
 export class Header {
-  constructor(readonly appStore: AppStore) {
-    console.log(this.appStore.isLoggedIn());
-  }
+  constructor(
+    readonly appStore: AppStore,
+    private readonly router: Router,
+  ) {}
 
+  logout(): void {
+    this.appStore.logout();
+    this.router.navigate(['/login']);
+  }
 }
